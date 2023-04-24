a3fb945e1668f50e93e7bf08ba9262230edd3e4b
Selborn Anders <k000817@ad.smhi.se>
2023-04-24 11:24:11 +0200

    Profilbild, kontohantering etc.

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -13,5 +13,8 @@ app.app_context().push()
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
+login_manager.login_view = 'login' # function name of the route
+login_manager.login_message_category = 'info' # maps to bootstrap ?
+
 
 from flaskblog import routes
--- flaskblog/models.py
+++ flaskblog/models.py
@@ -1,21 +1,24 @@
 from datetime import datetime
-from flaskblog import db, login_manager
+from flaskblog import db, login_manager, app
 from flask_login import UserMixin
 
 @login_manager.user_loader
 def load_user(user_id):
-    return User.query.get(int())
+    return User.query.get(int(user_id))
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key = True)
     username = db.Column(db.String(20), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    image_file = db.Column(db.String(20), nullable=False, default='default.jpeg')
+    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
     password = db.Column(db.String(60), nullable=False)
     posts = db.relationship('Post', backref='author', lazy=True) # post attribute has relationship to Post model. Lazy defines when Alchemy loads data from database.
 
     def __rep__(self):
         return f"User('{self.username}, {self.email}, {self.image_file}')"
+    
+    # with app.app_context():
+    #    db.create_all()
 
 
 class Post(db.Model):
@@ -27,4 +30,7 @@ class Post(db.Model):
 
     def __rep__(self):
         return f"User('{self.title}, {self.date_posted}')"
+    
+    # with app.app_context():
+    #    db.create_all() 
 
