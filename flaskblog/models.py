6c3dbaf7b6b3f2add5dd5938edc35831da054a92
SMHI\k000817 <anders.selborn@smhi.se>
2023-04-26 11:58:02 +0200

    Request new password. Email functionality

--- flaskblog/models.py
+++ flaskblog/models.py
@@ -1,6 +1,8 @@
+from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 from datetime import datetime
 from flaskblog import db, login_manager, app
 from flask_login import UserMixin
+
 
 @login_manager.user_loader
 def load_user(user_id):
@@ -14,9 +16,25 @@ class User(db.Model, UserMixin):
     password = db.Column(db.String(60), nullable=False)
     posts = db.relationship('Post', backref='author', lazy=True) # post attribute has relationship to Post model. Lazy defines when Alchemy loads data from database.
 
+    def get_reset_token(self, expires_seconds=1800):
+        s = Serializer(app.config['SECRET_KEY'], expires_seconds)
+        return s.dumps({'user_id': self.id}).decode('utf-8')
+    
+
+    @staticmethod
+    def verify_reset_token(token):
+        s = Serializer(app.config['SECRET_KEY'])
+        try:
+            user_id = s.loads(token)['user_id']
+        except:
+            return None
+        return User.query.get(user_id)
+
+
     def __rep__(self):
         return f"User('{self.username}, {self.email}, {self.image_file}')"
     
+
     with app.app_context():
        db.create_all()
 
